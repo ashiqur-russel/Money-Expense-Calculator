821b6f21075117ceb360e7be26a69892463cd99f
ashiqurras <ashiqurrussel2@gmail.com>
2022-08-20 19:46:58 +0200

    balance update

--- js/app.js
+++ js/app.js
@@ -1,17 +1,17 @@
 document.getElementById("calculation").addEventListener("click", () => {
-  getIncomeAmount();
+  const income = getIncomeAmount();
+  console.log(income);
 
-  const foodExpense = getInputElementById("food");
-  const rentExpense = getInputElementById("rent");
-  const clothesExpenese = getInputElementById("clothes");
-  if (isNaN(foodExpense) || isNaN(rentExpense) || isNaN(clothesExpenese)) {
-    alert("Please Enter value");
-    return false;
-  }
-  const totalExpense = foodExpense + rentExpense + clothesExpenese;
+  //Change expense field
+  const totalExpense = calculateExpense();
+  console.log(totalExpense);
   const previousTotalExpense = getTextElementById("total-expense");
   const newTotalExpense = previousTotalExpense + totalExpense;
   document.getElementById("total-expense").innerText = newTotalExpense;
+
+  //Balance update
+  const updateBalance = income - totalExpense;
+  document.getElementById("balance").innerText = updateBalance;
 });
 
 function getIncomeAmount() {
@@ -27,9 +27,30 @@ function getInputElementById(elemntId) {
   return inputFieldValue;
 }
 
+function calculateExpense() {
+  //Calculate expense
+  const foodExpense = getInputElementById("food");
+  const rentExpense = getInputElementById("rent");
+  const clothesExpenese = getInputElementById("clothes");
+  if (isNaN(foodExpense) || isNaN(rentExpense) || isNaN(clothesExpenese)) {
+    alert("Please Enter value");
+    return false;
+  }
+  const totalExpense = foodExpense + rentExpense + clothesExpenese;
+
+  return totalExpense;
+}
+
 function getTextElementById(elemntId) {
   const textElement = document.getElementById(elemntId);
   const textElementString = textElement.innerText;
   const textElementNumber = parseFloat(textElementString);
   return textElementNumber;
+}
+
+function updateBalance(income, expense) {
+  const incomeBalance = income;
+  const expenseBalance = expense;
+  const newBalance = incomeBalance - expenseBalance;
+  console.log(newBalance);
 }
